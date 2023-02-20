03cf77c79711f3769e26cefc446deb47c1edb77b
Md Shamim <shamimice03@gmail.com>
2023-02-20 16:14:09 +0900

    Create lambda-function-BuckeLists.py

---
+++ Invoke-lambda-function/lambda-function-BuckeLists.py
@@ -0,0 +1,15 @@
+import json
+import boto3
+
+s3 = boto3.resource('s3')
+
+def lambda_handler(event, context):
+  bucketlist = []
+  for bucket in s3.buckets.all():
+      bucketlist.append(bucket.name)
+      
+  print(bucketlist)
+  return {
+      "statusCode": 200,
+      "body": bucketlist
+      }
